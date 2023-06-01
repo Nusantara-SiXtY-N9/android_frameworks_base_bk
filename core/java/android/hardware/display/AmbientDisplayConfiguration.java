yaap/frameworks_base

base: Allow setting wake for notification separately from doze [1/2]
The strings were misleading. Disabling wake for notifications actually entirely disables doze.
Add a new setting to configure wake for notification separately and amend doze switch strings accordingly

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -52,6 +52,7 @@ public class AmbientDisplayConfiguration {
             Settings.Secure.DOZE_WAKE_DISPLAY_GESTURE,
             Settings.Secure.DOZE_TAP_SCREEN_GESTURE,
             Settings.Secure.DOZE_ON_CHARGE,
+            Settings.Secure.DOZE_FOR_NOTIFICATIONS,
     };
 
     /** Non-user configurable doze settings */
@@ -92,6 +93,12 @@ public class AmbientDisplayConfiguration {
     public boolean pulseOnNotificationAvailable() {
         return mContext.getResources().getBoolean(R.bool.config_pulseOnNotificationsAvailable)
                 && ambientDisplayAvailable();
+    }
+
+    /** @hide */
+    public boolean userPulseOnNotificationEnabled(int user) {
+        return boolSettingDefaultOn(Settings.Secure.DOZE_FOR_NOTIFICATIONS, user)
+                && pulseOnNotificationEnabled(user);
     }
 
     /** @hide */

Nusantara-SiXtY-N9/android_frameworks_base_bk

Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -56,7 +56,8 @@ public class AmbientDisplayConfiguration {
             Settings.Secure.DOZE_WAKE_LOCK_SCREEN_GESTURE,
             Settings.Secure.DOZE_WAKE_DISPLAY_GESTURE,
             Settings.Secure.DOZE_TAP_SCREEN_GESTURE,
-            Settings.Secure.DOZE_ON_CHARGE
+            Settings.Secure.DOZE_ON_CHARGE,
+            Settings.Secure.DOZE_FOR_NOTIFICATIONS
     };
 
     /** Non-user configurable doze settings */
@@ -99,6 +100,12 @@ public class AmbientDisplayConfiguration {
     public boolean pulseOnNotificationAvailable() {
         return mContext.getResources().getBoolean(R.bool.config_pulseOnNotificationsAvailable)
                 && ambientDisplayAvailable();
+    }
+    
+    /** @hide */
+    public boolean userPulseOnNotificationEnabled(int user) {
+        return boolSettingDefaultOn(Settings.Secure.DOZE_FOR_NOTIFICATIONS, user)
+                && pulseOnNotificationEnabled(user);
     }
 
     /** @hide */